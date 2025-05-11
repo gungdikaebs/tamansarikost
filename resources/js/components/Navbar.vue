84ae77ab7ea314db09bfea3c748df8a916fca7ec
gungdikaebs <gungdika88@gmail.com>
2025-05-11 12:47:19 +0800

    first commit

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav class="
+        mx-auto flex flex-col gap-3 bg-white pb-10
+        md:flex-row md:justify-between  md:pb-0  md:p-5 md:w-7xl">
+        <div class="text-2xl font-bold text-cyan-50 bg-[#2E5DE8] md:bg-white md:text-[#2E5DE8]
+ w-full p-5">.TamanSariKost</div>
+        <ul class="flex flex-col md:flex-row gap-3 pl-5  md:px-2 md:gap-10 w-full md:items-center">
+            <li>
+                <a href="#" class="text-gray-600 hover:text-[#2E5DE8] text-lg ">Home</a>
+            </li>
+            <li>
+                <a href="#" class="text-gray-600 hover:text-[#2E5DE8] text-lg ">About</a>
+            </li>
+            <li>
+                <a href="#" class="text-gray-600 hover:text-[#2E5DE8] text-lg ">Services</a>
+            </li>
+            <li>
+                <a href="#" class="text-gray-600 hover:text-[#2E5DE8] text-lg ">Contact</a>
+            </li>
+            <li>
+                <a href="" class="text-lg text-cyan-50 mt-20 bg-[#2E5DE8] px-4 py-2 rounded-[14px]"> Book Now</a>
+            </li>
+        </ul>
+
+    </nav>
+
+</template>
